07f6f63e449a31ca747fedcf7fd7dab423e1614e
dannyaguilargil <danny.aguilar.gil@gmail.com>
2024-02-22 17:06:52 -0500

    inicialozando proyecto

--- observatorio/observatorio/settings.py
+++ observatorio/observatorio/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #############################
+    'rest_framework',
+    
+    
+    
 ]
 
 MIDDLEWARE = [
@@ -103,9 +108,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-ar'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Buenos_Aires'
 
 USE_I18N = True
 
